0b099c0d420e3f886e56c09f13661aff4b0d2b4a
k0michi <k0michi@koyomi.co>
2025-09-20 20:09:36 +0900

    Implement context sharing

--- src/kl/graphics/opengl/gl_context.cc
+++ src/kl/graphics/opengl/gl_context.cc
@@ -33,8 +33,17 @@ SDL_GLContext GLContext::sdlContext() const noexcept { return mContext; }
 std::recursive_mutex &GLContext::mutex() noexcept { return mMutex; }
 
 std::expected<std::shared_ptr<GLContext>, std::runtime_error>
-GLContext::create(SDL_Window *window) noexcept {
+GLContext::create(SDL_Window *window,
+                  std::shared_ptr<GLContext> shareContext) noexcept {
+  if (shareContext) {
+    SDL_GL_MakeCurrent(shareContext->sdlWindow(), shareContext->sdlContext());
+    SDL_GL_SetAttribute(SDL_GL_SHARE_WITH_CURRENT_CONTEXT, 1);
+  } else {
+    SDL_GL_SetAttribute(SDL_GL_SHARE_WITH_CURRENT_CONTEXT, 0);
+  }
+
   auto sdlContext = SDL_GL_CreateContext(window);
+  SDL_GL_MakeCurrent(nullptr, nullptr);
 
   if (!sdlContext) {
     return std::unexpected(std::runtime_error(SDL_GetError()));
