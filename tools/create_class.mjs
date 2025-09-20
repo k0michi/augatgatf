ad384dd925b861121da50804e73e5f914976f484
k0michi <k0michi@koyomi.co>
2025-09-20 17:01:31 +0900

    Fix typo

--- tools/create_class.mjs
+++ tools/create_class.mjs
@@ -106,7 +106,7 @@ function generateHeader({
         content += `class ${basename} {\n`;
         content += `public:\n`;
         content += `    explicit ${basename}() noexcept = default;\n`;
-        context += `    ${basename}(${basename}&&) noexcept = default;\n`;
+        content += `    ${basename}(${basename}&&) noexcept = default;\n`;
         content += `    virtual ~${basename}() noexcept = default;\n`;
         content += `\n`;
         content += `    ${basename}(const ${basename}&) = delete;\n`;
