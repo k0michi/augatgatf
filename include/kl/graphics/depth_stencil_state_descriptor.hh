25d3a7722c607aceea4f78fa32421052d3f12979
k0michi <k0michi@koyomi.co>
2025-09-21 22:51:09 +0900

    Set default in DepthStencilStateDescriptor

--- include/kl/graphics/depth_stencil_state_descriptor.hh
+++ include/kl/graphics/depth_stencil_state_descriptor.hh
@@ -36,28 +36,28 @@ enum class StencilOp {
  * https://registry.khronos.org/vulkan/specs/latest/man/html/VkStencilOpState.html
  */
 struct StencilOpStateDescriptor final {
-  StencilOp failOp;
-  StencilOp passOp;
-  StencilOp depthFailOp;
-  StencilOp compareOp;
-  uint32_t compareMask;
-  uint32_t writeMask;
-  uint32_t reference;
+  StencilOp failOp = StencilOp::eKeep;
+  StencilOp passOp = StencilOp::eKeep;
+  StencilOp depthFailOp = StencilOp::eKeep;
+  CompareOp compareOp = CompareOp::eAlways;
+  uint32_t compareMask = 0xFFFFFFFF;
+  uint32_t writeMask = 0xFFFFFFFF;
+  uint32_t reference = 0;
 };
 
 /**
  * https://registry.khronos.org/vulkan/specs/latest/man/html/VkPipelineDepthStencilStateCreateInfo.html
  */
 struct DepthStencilStateDescriptor final {
-  bool depthTestEnable;
-  bool depthWriteEnable;
-  CompareOp depthCompareOp;
-  bool depthBoundsTestEnable;
-  bool stencilTestEnable;
-  StencilOpStateDescriptor front;
-  StencilOpStateDescriptor back;
-  float minDepthBounds;
-  float maxDepthBounds;
+  bool depthTestEnable = false;
+  bool depthWriteEnable = true;
+  CompareOp depthCompareOp = CompareOp::eLess;
+  bool depthBoundsTestEnable = false;
+  bool stencilTestEnable = false;
+  StencilOpStateDescriptor front = {};
+  StencilOpStateDescriptor back = {};
+  float minDepthBounds = 0.0f;
+  float maxDepthBounds = 1.0f;
 };
 } // namespace kl::graphics
 #endif // KL_GRAPHICS_DEPTH_STENCIL_STATE_DESCRIPTOR_HH
