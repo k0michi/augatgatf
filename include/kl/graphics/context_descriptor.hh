5f73350d29873b568e2d61a0e02d0c0fc297acc6
k0michi <k0michi@koyomi.co>
2025-09-21 10:32:16 +0900

    Add ContextDescriptor

---
+++ include/kl/graphics/context_descriptor.hh
@@ -0,0 +1,7 @@
+#ifndef KL_GRAPHICS_CONTEXT_DESCRIPTOR_HH
+#define KL_GRAPHICS_CONTEXT_DESCRIPTOR_HH
+
+namespace kl::graphics {
+struct ContextDescriptor final {};
+} // namespace kl::graphics
+#endif // KL_GRAPHICS_CONTEXT_DESCRIPTOR_HH
