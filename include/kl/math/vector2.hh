90a5a7deea0c3a2ca47b9d08f033917c9aa8c364
k0michi <k0michi@koyomi.co>
2025-09-22 17:36:36 +0900

    Use concept in Vector

--- include/kl/math/vector2.hh
+++ include/kl/math/vector2.hh
@@ -2,43 +2,49 @@
 #define KL_MATH_VECTOR2_HH
 
 #include <cmath>
+#include <concepts>
+#include <ostream>
 #include <stdexcept>
+#include <typeinfo>
 
 namespace kl::math {
 template <typename T, std::size_t N> struct Vector;
 
-constexpr Vector<float, 2> operator+(Vector<float, 2> lhs,
-                                     const Vector<float, 2> &rhs) noexcept;
-constexpr Vector<float, 2> operator-(Vector<float, 2> lhs,
-                                     const Vector<float, 2> &rhs) noexcept;
-constexpr Vector<float, 2> operator*(float lhs, Vector<float, 2> rhs) noexcept;
+template <std::floating_point T>
+constexpr Vector<T, 2> operator+(Vector<T, 2> lhs,
+                                 const Vector<T, 2> &rhs) noexcept;
+template <std::floating_point T>
+constexpr Vector<T, 2> operator-(Vector<T, 2> lhs,
+                                 const Vector<T, 2> &rhs) noexcept;
+template <std::floating_point T>
+constexpr Vector<T, 2> operator*(T lhs, Vector<T, 2> rhs) noexcept;
 
-template <> struct Vector<float, 2> final {
-  float x;
-  float y;
+template <std::floating_point T> struct Vector<T, 2> final {
+  T x;
+  T y;
 
-  static constexpr Vector<float, 2> zero() noexcept {
-    return Vector<float, 2>{0.0f, 0.0f};
+  static constexpr Vector<T, 2> zero() noexcept {
+    return Vector<T, 2>{0.0f, 0.0f};
   }
 
-  static constexpr Vector<float, 2> one() noexcept {
-    return Vector<float, 2>{1.0f, 1.0f};
+  static constexpr Vector<T, 2> one() noexcept {
+    return Vector<T, 2>{1.0f, 1.0f};
   }
 
-  static constexpr Vector<float, 2> unitX() noexcept {
-    return Vector<float, 2>{1.0f, 0.0f};
+  static constexpr Vector<T, 2> unitX() noexcept {
+    return Vector<T, 2>{1.0f, 0.0f};
   }
 
-  static constexpr Vector<float, 2> unitY() noexcept {
-    return Vector<float, 2>{0.0f, 1.0f};
+  static constexpr Vector<T, 2> unitY() noexcept {
+    return Vector<T, 2>{0.0f, 1.0f};
   }
 
-  constexpr float lengthSquared() const noexcept { return x * x + y * y; }
+  constexpr T lengthSquared() const noexcept { return x * x + y * y; }
 
-  constexpr float length() const noexcept { return std::sqrt(lengthSquared()); }
+  constexpr T length() const noexcept { return std::sqrt(lengthSquared()); }
 
   constexpr void normalize() noexcept {
-    float len = length();
+    T len = length();
 
     if (len == 0.0f) {
       (*this) = zero();
@@ -48,39 +54,38 @@ template <> struct Vector<float, 2> final {
     (*this) /= len;
   }
 
-  static constexpr Vector<float, 2>
-  normalize(const Vector<float, 2> &v) noexcept {
-    Vector<float, 2> result = v;
+  static constexpr Vector<T, 2> normalize(const Vector<T, 2> &v) noexcept {
+    Vector<T, 2> result = v;
     result.normalize();
     return result;
   }
 
-  static constexpr float dot(const Vector<float, 2> &a,
-                             const Vector<float, 2> &b) noexcept {
+  static constexpr T dot(const Vector<T, 2> &a,
+                         const Vector<T, 2> &b) noexcept {
     return a.x * b.x + a.y * b.y;
   }
 
-  static constexpr float cross(const Vector<float, 2> &a,
-                               const Vector<float, 2> &b) noexcept {
+  static constexpr T cross(const Vector<T, 2> &a,
+                           const Vector<T, 2> &b) noexcept {
     return a.x * b.y - a.y * b.x;
   }
 
-  static constexpr float distance(const Vector<float, 2> &a,
-                                  const Vector<float, 2> &b) noexcept {
+  static constexpr T distance(const Vector<T, 2> &a,
+                              const Vector<T, 2> &b) noexcept {
     return (a - b).length();
   }
 
-  static constexpr float distanceSquared(const Vector<float, 2> &a,
-                                         const Vector<float, 2> &b) noexcept {
+  static constexpr T distanceSquared(const Vector<T, 2> &a,
+                                     const Vector<T, 2> &b) noexcept {
     return (a - b).lengthSquared();
   }
 
-  static constexpr Vector<float, 2>
-  lerp(const Vector<float, 2> &a, const Vector<float, 2> &b, float t) noexcept {
+  static constexpr Vector<T, 2> lerp(const Vector<T, 2> &a,
+                                     const Vector<T, 2> &b, T t) noexcept {
     return a + t * (b - a);
   }
 
-  constexpr float &operator[](std::size_t index) {
+  constexpr T &operator[](std::size_t index) {
     switch (index) {
     case 0:
       return x;
@@ -91,107 +96,120 @@ template <> struct Vector<float, 2> final {
     }
   }
 
-  constexpr Vector<float, 2> &
-  operator+=(const Vector<float, 2> &other) noexcept {
+  constexpr Vector<T, 2> &operator+=(const Vector<T, 2> &other) noexcept {
     x += other.x;
     y += other.y;
     return *this;
   }
 
-  constexpr Vector<float, 2> &
-  operator-=(const Vector<float, 2> &other) noexcept {
+  constexpr Vector<T, 2> &operator-=(const Vector<T, 2> &other) noexcept {
     x -= other.x;
     y -= other.y;
     return *this;
   }
 
-  constexpr Vector<float, 2> &operator*=(float other) noexcept {
+  constexpr Vector<T, 2> &operator*=(T other) noexcept {
     x *= other;
     y *= other;
     return *this;
   }
 
-  constexpr Vector<float, 2> &
-  operator*=(const Vector<float, 2> &other) noexcept {
+  constexpr Vector<T, 2> &operator*=(const Vector<T, 2> &other) noexcept {
     x *= other.x;
     y *= other.y;
     return *this;
   }
 
-  constexpr Vector<float, 2> &operator/=(float other) noexcept {
+  constexpr Vector<T, 2> &operator/=(T other) noexcept {
     x /= other;
     y /= other;
     return *this;
   }
 
-  constexpr Vector<float, 2> &
-  operator/=(const Vector<float, 2> &other) noexcept {
+  constexpr Vector<T, 2> &operator/=(const Vector<T, 2> &other) noexcept {
     x /= other.x;
     y /= other.y;
     return *this;
   }
 
-  constexpr Vector<float, 2> operator-() const noexcept {
-    return Vector<float, 2>{-x, -y};
+  constexpr Vector<T, 2> operator-() const noexcept {
+    return Vector<T, 2>{-x, -y};
   }
 };
 
-constexpr Vector<float, 2> operator+(Vector<float, 2> lhs,
-                                     const Vector<float, 2> &rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator+(Vector<T, 2> lhs,
+                                 const Vector<T, 2> &rhs) noexcept {
   lhs += rhs;
   return lhs;
 }
 
-constexpr Vector<float, 2> operator-(Vector<float, 2> lhs,
-                                     const Vector<float, 2> &rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator-(Vector<T, 2> lhs,
+                                 const Vector<T, 2> &rhs) noexcept {
   lhs -= rhs;
   return lhs;
 }
 
-constexpr Vector<float, 2> operator*(Vector<float, 2> lhs, float rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator*(Vector<T, 2> lhs, float rhs) noexcept {
   lhs *= rhs;
   return lhs;
 }
 
-constexpr Vector<float, 2> operator*(float lhs, Vector<float, 2> rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator*(float lhs, Vector<T, 2> rhs) noexcept {
   rhs *= lhs;
   return rhs;
 }
 
-constexpr Vector<float, 2> operator*(Vector<float, 2> lhs,
-                                     const Vector<float, 2> &rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator*(Vector<T, 2> lhs,
+                                 const Vector<T, 2> &rhs) noexcept {
   lhs *= rhs;
   return lhs;
 }
 
-constexpr Vector<float, 2> operator/(Vector<float, 2> lhs, float rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator/(Vector<T, 2> lhs, T rhs) noexcept {
   lhs /= rhs;
   return lhs;
 }
 
-constexpr Vector<float, 2> operator/(Vector<float, 2> lhs,
-                                     const Vector<float, 2> &rhs) noexcept {
+template <std::floating_point T>
+constexpr Vector<T, 2> operator/(Vector<T, 2> lhs,
+                                 const Vector<T, 2> &rhs) noexcept {
   lhs /= rhs;
   return lhs;
 }
 
-constexpr Vector<float, 2> operator/(float lhs,
-                                     const Vector<float, 2> &rhs) noexcept {
-  Vector<float, 2> result{lhs, lhs};
+template <std::floating_point T>
+constexpr Vector<T, 2> operator/(T lhs, const Vector<T, 2> &rhs) noexcept {
+  Vector<T, 2> result{lhs, lhs};
   result /= rhs;
   return result;
 }
 
-constexpr bool operator==(const Vector<float, 2> &lhs,
-                          const Vector<float, 2> &rhs) noexcept {
+template <std::floating_point T>
+constexpr bool operator==(const Vector<T, 2> &lhs,
+                          const Vector<T, 2> &rhs) noexcept {
   return lhs.x == rhs.x && lhs.y == rhs.y;
 }
 
-constexpr bool operator!=(const Vector<float, 2> &lhs,
-                          const Vector<float, 2> &rhs) noexcept {
+template <std::floating_point T>
+constexpr bool operator!=(const Vector<T, 2> &lhs,
+                          const Vector<T, 2> &rhs) noexcept {
   return !(lhs == rhs);
 }
 
+template <std::floating_point T>
+std::ostream &operator<<(std::ostream &os, const Vector<T, 2> &vec) {
+  auto name = typeid(T).name();
+  os << "Vector<" << name << ", 2>{" << vec.x << ", " << vec.y << "}";
+  return os;
+}
+
 using Vector2 = Vector<float, 2>;
+using DVector2 = Vector<double, 2>;
 } // namespace kl::math
 #endif // KL_MATH_VECTOR<FLOAT, 2>_HH
