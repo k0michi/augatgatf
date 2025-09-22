4c7ce965c47490647ddfed5a340cb2ab16685ac5
k0michi <k0michi@koyomi.co>
2025-09-22 21:48:56 +0900

    Perform no initialization by default in Vectors

--- test/test_vector2.cc
+++ test/test_vector2.cc
@@ -2,13 +2,7 @@
 
 #include <kl/math/vector2.hh>
 
-TEST(Vector2, DefaultConstructor) {
-  kl::math::Vector2 v;
-  EXPECT_FLOAT_EQ(v.x, 0.0f);
-  EXPECT_FLOAT_EQ(v.y, 0.0f);
-}
-
-TEST(Vector2, ParameterizedConstructor) {
+TEST(Vector2, Constructor) {
   kl::math::Vector2 v{3.0f, 4.0f};
   EXPECT_FLOAT_EQ(v.x, 3.0f);
   EXPECT_FLOAT_EQ(v.y, 4.0f);
