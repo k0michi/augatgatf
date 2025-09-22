1c38f78577f7d3334a64591cd84f7b4be1605faf
k0michi <k0michi@koyomi.co>
2025-09-22 18:06:54 +0900

    Fix Matrix2x2

--- include/kl/math/matrix2x2.hh
+++ include/kl/math/matrix2x2.hh
@@ -8,46 +8,48 @@
 namespace kl::math {
 template <typename T, std::size_t C, std::size_t R> struct Matrix;
 
-template <> struct Matrix<float, 2, 2> final {
+template <std::floating_point T> struct Matrix<T, 2, 2> final {
   union {
     struct {
-      Vector2 col0;
-      Vector2 col1;
+      Vector<T, 2> col0;
+      Vector<T, 2> col1;
     };
     struct {
-      float m00, m10;
-      float m01, m11;
+      T m00, m01;
+      T m10, m11;
     };
   };
 
-  static constexpr Matrix<float, 2, 2> zero() noexcept {
-    return Matrix<float, 2, 2>{Vector2::zero(), Vector2::zero()};
+  Matrix<T, 2, 2>(const Vector<T, 2> &col0, const Vector<T, 2> &col1)
+      : col0(col0), col1(col1) {}
+  Matrix<T, 2, 2>(T m00, T m01, T m10, T m11)
+      : m00(m00), m01(m01), m10(m10), m11(m11) {}
+
+  static constexpr Matrix<T, 2, 2> zero() noexcept {
+    return Matrix<T, 2, 2>{Vector2::zero(), Vector2::zero()};
   }
 
-  static constexpr Matrix<float, 2, 2> identity() noexcept {
-    return Matrix<float, 2, 2>{Vector2::unitX(), Vector2::unitY()};
+  static constexpr Matrix<T, 2, 2> identity() noexcept {
+    return Matrix<T, 2, 2>{Vector2::unitX(), Vector2::unitY()};
   }
 
-  constexpr float determinant() const noexcept {
-    return col0.x * col1.y - col0.y * col1.x;
-  }
+  constexpr T determinant() const noexcept { return m00 * m11 - m01 * m10; }
 
-  constexpr std::optional<Matrix<float, 2, 2>> inverse() const noexcept {
-    float det = determinant();
+  constexpr std::optional<Matrix<T, 2, 2>> inverse() const noexcept {
+    T det = determinant();
 
-    if (det == 0.0f) {
+    if (det == T(0)) {
       return std::nullopt;
     }
 
-    float invDet = 1.0f / det;
+    T invDet = T(1) / det;
 
-    return Matrix<float, 2, 2>{Vector2{col1.y * invDet, -col0.y * invDet},
-                               Vector2{-col1.x * invDet, col0.x * invDet}};
+    return Matrix<T, 2, 2>{m11 * invDet, -m01 * invDet, -m10 * invDet,
+                           m00 * invDet};
   }
 
-  constexpr Matrix<float, 2, 2> transpose() noexcept {
-    return Matrix<float, 2, 2>{Vector2{col0.x, col1.x},
-                               Vector2{col0.y, col1.y}};
+  constexpr Matrix<T, 2, 2> transpose() noexcept {
+    return Matrix<T, 2, 2>{m00, m10, m01, m11};
   };
 
   constexpr Vector2 &operator[](std::size_t index) {
@@ -61,42 +63,99 @@ template <> struct Matrix<float, 2, 2> final {
     }
   }
 
-  constexpr Matrix<float, 2, 2> &
-  operator+=(const Matrix<float, 2, 2> &other) noexcept {
+  constexpr Matrix<T, 2, 2> &operator+=(const Matrix<T, 2, 2> &other) noexcept {
     col0 += other.col0;
     col1 += other.col1;
     return *this;
   }
 
-  constexpr Matrix<float, 2, 2> &
-  operator-=(const Matrix<float, 2, 2> &other) noexcept {
+  constexpr Matrix<T, 2, 2> &operator-=(const Matrix<T, 2, 2> &other) noexcept {
     col0 -= other.col0;
     col1 -= other.col1;
     return *this;
   }
 
-  constexpr Matrix<float, 2, 2> &operator*=(float other) noexcept {
+  constexpr Matrix<T, 2, 2> &operator*=(float other) noexcept {
     col0 *= other;
     col1 *= other;
     return *this;
   }
 
-  constexpr Matrix<float, 2, 2> &
-  operator*=(const Matrix<float, 2, 2> &other) noexcept {
-    *this =
-        Matrix<float, 2, 2>{Vector2{col0.x * other.m00 + col1.x * other.m01,
-                                    col0.y * other.m00 + col1.y * other.m01},
-                            Vector2{col0.x * other.m10 + col1.x * other.m11,
-                                    col0.y * other.m10 + col1.y * other.m11}};
+  constexpr Vector<T, 2> operator*(const Vector<T, 2> &vec) const noexcept {
+    return Vector<T, 2>{m00 * vec.x + m01 * vec.y, m10 * vec.x + m11 * vec.y};
+  }
+
+  constexpr Matrix<T, 2, 2> &operator*=(const Matrix<T, 2, 2> &other) noexcept {
+    *this = Matrix<T, 2, 2>{
+        m00 * other.m00 + m01 * other.m10, m00 * other.m01 + m01 * other.m11,
+        m10 * other.m00 + m11 * other.m10, m10 * other.m01 + m11 * other.m11};
     return *this;
   }
 
-  constexpr Matrix<float, 2, 2> &operator/=(float other) noexcept {
+  constexpr Matrix<T, 2, 2> &operator/=(float other) noexcept {
     col0 /= other;
     col1 /= other;
     return *this;
   }
+
+  constexpr Matrix<T, 2, 2> operator-() const noexcept {
+    return Matrix<T, 2, 2>{-col0, -col1};
+  }
+
+  template <typename U>
+    requires(std::is_convertible_v<T, U>)
+  constexpr operator Matrix<U, 2, 2>() const noexcept {
+    return Matrix<U, 2, 2>{static_cast<U>(m00), static_cast<U>(m01),
+                           static_cast<U>(m10), static_cast<U>(m11)};
+  }
 };
+
+template <std::floating_point T>
+constexpr Matrix<T, 2, 2> operator+(Matrix<T, 2, 2> lhs,
+                                    const Matrix<T, 2, 2> &rhs) noexcept {
+  lhs += rhs;
+  return lhs;
+}
+
+template <std::floating_point T>
+constexpr Matrix<T, 2, 2> operator-(Matrix<T, 2, 2> lhs,
+                                    const Matrix<T, 2, 2> &rhs) noexcept {
+  lhs -= rhs;
+  return lhs;
+}
+
+template <std::floating_point T>
+constexpr Matrix<T, 2, 2> operator*(Matrix<T, 2, 2> lhs, float rhs) noexcept {
+  lhs *= rhs;
+  return lhs;
+}
+
+template <std::floating_point T>
+constexpr Matrix<T, 2, 2> operator*(float lhs, Matrix<T, 2, 2> rhs) noexcept {
+  rhs *= lhs;
+  return rhs;
+}
+
+template <std::floating_point T>
+constexpr Matrix<T, 2, 2> operator*(Matrix<T, 2, 2> lhs,
+                                    const Matrix<T, 2, 2> &rhs) noexcept {
+  lhs *= rhs;
+  return lhs;
+}
+
+template <std::floating_point T>
+constexpr Matrix<T, 2, 2> operator/(Matrix<T, 2, 2> lhs, float rhs) noexcept {
+  lhs /= rhs;
+  return lhs;
+}
+
+template <std::floating_point T>
+std::ostream &operator<<(std::ostream &os, const Matrix<T, 2, 2> &mat) {
+  auto name = typeid(T).name();
+  os << "Matrix<" << name << ", 2, 2>{" << mat.m00 << ", " << mat.m01 << "; "
+     << mat.m10 << ", " << mat.m11 << "}";
+  return os;
+}
 
 using Matrix2x2 = Matrix<float, 2, 2>;
 using Matrix2 = Matrix<float, 2, 2>;
