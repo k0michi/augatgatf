7075331b180678182406662357bd5316190012be
k0michi <k0michi@koyomi.co>
2025-09-23 19:22:44 +0900

    Add TextureUsage

--- include/kl/graphics/texture_descriptor.hh
+++ include/kl/graphics/texture_descriptor.hh
@@ -8,12 +8,82 @@
 #include "kl/common/extent3.hh"
 
 namespace kl::graphics {
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkImageType.html
+ */
 enum class TextureType {
   e1D,
   e2D,
   e3D,
 };
 
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkImageUsageFlagBits.html
+ */
+enum class TextureUsage : uint32_t {
+  /**
+   * Corresponds to WGPUTextureUsage_CopySrc.
+   */
+  eTransferSrc = 0x00000001,
+  /**
+   * Corresponds to WGPUTextureUsage_CopyDst.
+   */
+  eTransferDst = 0x00000002,
+  /**
+   * Corresponds to WGPUTextureUsage_TextureBinding.
+   */
+  eSampled = 0x00000004,
+  /**
+   * Corresponds to WGPUTextureUsage_StorageBinding.
+   */
+  eStorage = 0x00000008,
+  /**
+   * Corresponds to WGPUTextureUsage_RenderAttachment.
+   */
+  eColorAttachment = 0x00000010,
+  /**
+   * Corresponds to WGPUTextureUsage_RenderAttachment.
+   */
+  eDepthStencilAttachment = 0x00000020,
+};
+
+constexpr TextureUsage operator|(TextureUsage lhs, TextureUsage rhs) {
+  return static_cast<TextureUsage>(static_cast<uint32_t>(lhs) |
+                                   static_cast<uint32_t>(rhs));
+}
+
+constexpr TextureUsage operator&(TextureUsage lhs, TextureUsage rhs) {
+  return static_cast<TextureUsage>(static_cast<uint32_t>(lhs) &
+                                   static_cast<uint32_t>(rhs));
+}
+
+constexpr TextureUsage operator^(TextureUsage lhs, TextureUsage rhs) {
+  return static_cast<TextureUsage>(static_cast<uint32_t>(lhs) ^
+                                   static_cast<uint32_t>(rhs));
+}
+
+constexpr TextureUsage operator~(TextureUsage usage) {
+  return static_cast<TextureUsage>(~static_cast<uint32_t>(usage));
+}
+
+constexpr TextureUsage &operator|=(TextureUsage &lhs, TextureUsage rhs) {
+  lhs = lhs | rhs;
+  return lhs;
+}
+
+constexpr TextureUsage &operator&=(TextureUsage &lhs, TextureUsage rhs) {
+  lhs = lhs & rhs;
+  return lhs;
+}
+
+constexpr TextureUsage &operator^=(TextureUsage &lhs, TextureUsage rhs) {
+  lhs = lhs ^ rhs;
+  return lhs;
+}
+
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkImageCreateInfo.html
+ */
 struct TextureDescriptor final {
   TextureType type = TextureType::e2D;
   Format format = Format::eR8G8B8A8Unorm;
@@ -21,6 +91,7 @@ struct TextureDescriptor final {
   std::optional<uint32_t> mipLevels;
   uint32_t arrayLayers = 1;
   uint32_t samples = 1;
+  TextureUsage usage = TextureUsage::eSampled | TextureUsage::eTransferDst;
 };
 } // namespace kl::graphics
 #endif // KL_GRAPHICS_TEXTURE_DESCRIPTOR_HH
