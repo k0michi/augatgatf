2257d66f43b32562d5203c04b7abb2739538a795
k0michi <k0michi@koyomi.co>
2025-09-23 21:49:54 +0900

    Add SamplerDescriptor

---
+++ include/kl/graphics/sampler_descriptor.hh
@@ -0,0 +1,75 @@
+#ifndef KL_GRAPHICS_SAMPLER_DESCRIPTOR_HH
+#define KL_GRAPHICS_SAMPLER_DESCRIPTOR_HH
+
+namespace kl::graphics {
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkFilter.html
+ */
+enum class Filter {
+  eNearest,
+  eLinear,
+};
+
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkSamplerMipmapMode.html
+ */
+enum class SamplerMipmapMode {
+  eNearest,
+  eLinear,
+};
+
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkSamplerAddressMode.html
+ */
+enum class SamplerAddressMode {
+  eRepeat,
+  eMirroredRepeat,
+  eClampToEdge,
+  eClampToBorder,
+  // eMirrorClampToEdge,
+};
+
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkCompareOp.html
+ */
+enum class CompareOp {
+  eNever,
+  eLess,
+  eEqual,
+  eLessOrEqual,
+  eGreater,
+  eNotEqual,
+  eGreaterOrEqual,
+  eAlways,
+};
+
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkBorderColor.html
+ */
+enum class BorderColor {
+  eTransparentBlack,
+  eOpaqueBlack,
+  eOpaqueWhite,
+};
+
+/**
+ * https://registry.khronos.org/vulkan/specs/latest/man/html/VkSamplerCreateInfo.html
+ */
+struct SamplerDescriptor final {
+  Filter magFilter = Filter::eLinear;
+  Filter minFilter = Filter::eLinear;
+  SamplerMipmapMode mipmapMode = SamplerMipmapMode::eLinear;
+  SamplerAddressMode addressModeU = SamplerAddressMode::eRepeat;
+  SamplerAddressMode addressModeV = SamplerAddressMode::eRepeat;
+  SamplerAddressMode addressModeW = SamplerAddressMode::eRepeat;
+  float mipLodBias = 0.0f;
+  bool anisotropyEnable = false;
+  float maxAnisotropy = 1.0f;
+  bool compareEnable = false;
+  CompareOp compareOp = CompareOp::eAlways;
+  float minLod = 0.0f;
+  float maxLod = 1000.0f;
+  BorderColor borderColor = BorderColor::eOpaqueBlack;
+};
+} // namespace kl::graphics
+#endif // KL_GRAPHICS_SAMPLER_DESCRIPTOR_HH
