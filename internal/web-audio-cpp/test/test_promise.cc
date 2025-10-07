3ec071d3c31d80b14a67929354f15daa8cfb56d2
k0michi <k0michi@koyomi.co>
2025-10-07 18:02:51 +0900

    Add Promise tests

---
+++ internal/web-audio-cpp/test/test_promise.cc
@@ -0,0 +1,77 @@
+#include <gtest/gtest.h>
+
+#include "web_audio.hh"
+
+TEST(PromiseTest, Then) {
+  web_audio::detail::EventQueue queue;
+  web_audio::Promise<int> promise(queue);
+  bool called = false;
+
+  promise.then([&](int value) {
+    called = true;
+    EXPECT_EQ(value, 42);
+  });
+
+  promise.getInternal()->resolve(42);
+  queue.poll();
+  EXPECT_TRUE(called);
+}
+
+TEST(PromiseTest, Catch) {
+  web_audio::detail::EventQueue queue;
+  web_audio::Promise<int> promise(queue);
+  bool called = false;
+
+  promise.catch_([&](std::exception_ptr exception) {
+    called = true;
+    try {
+      if (exception) {
+        std::rethrow_exception(exception);
+      }
+    } catch (const std::runtime_error &e) {
+      EXPECT_STREQ(e.what(), "Test error");
+    }
+  });
+
+  promise.getInternal()->reject(
+      std::make_exception_ptr(std::runtime_error("Test error")));
+  queue.poll();
+  EXPECT_TRUE(called);
+}
+
+TEST(PromiseTest, ThenAfterResolve) {
+  web_audio::detail::EventQueue queue;
+  web_audio::Promise<int> promise(queue);
+  bool called = false;
+
+  promise.getInternal()->resolve(42);
+
+  promise.then([&](int value) {
+    called = true;
+    EXPECT_EQ(value, 42);
+  });
+
+  EXPECT_TRUE(called);
+}
+
+TEST(PromiseTest, CatchAfterReject) {
+  web_audio::detail::EventQueue queue;
+  web_audio::Promise<int> promise(queue);
+  bool called = false;
+
+  promise.getInternal()->reject(
+      std::make_exception_ptr(std::runtime_error("Test error")));
+
+  promise.catch_([&](std::exception_ptr exception) {
+    called = true;
+    try {
+      if (exception) {
+        std::rethrow_exception(exception);
+      }
+    } catch (const std::runtime_error &e) {
+      EXPECT_STREQ(e.what(), "Test error");
+    }
+  });
+
+  EXPECT_TRUE(called);
+}
