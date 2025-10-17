ddd9eceff206043d9683ea8dd3f8ec9601714d9a
k0michi <k0michi@koyomi.co>
2025-10-17 22:25:38 +0900

    Refactor

--- internal/web-audio-cpp/include/web_audio/detail/upsampler.hh
+++ internal/web-audio-cpp/include/web_audio/detail/upsampler.hh
@@ -6,13 +6,15 @@
 #include "common.hh"
 
 namespace web_audio::detail {
-class Upsampler2x {
-public:
-  Upsampler2x(std::size_t blockSize);
+class Upsampler {
+  WEB_AUDIO_PROTECTED : Upsampler(const std::vector<float> &filterCoefficients,
+                                  std::size_t factor, std::size_t blockSize);
 
+public:
   void process(const std::vector<float> &input, std::vector<float> &output);
 
-  WEB_AUDIO_PRIVATE : std::size_t filterSize_;
+  WEB_AUDIO_PROTECTED : std::size_t factor_;
+  std::size_t filterSize_;
   std::size_t inputBlockSize_;
   std::size_t outputBlockSize_;
   std::size_t fftSize_;
@@ -20,5 +22,10 @@ public:
   std::vector<float> filterCoefficients_;
   std::vector<std::complex<float>> fftCoefficientsFrequency_;
   std::vector<std::complex<float>> overlapBuffer_;
+};
+
+class Upsampler2x : public Upsampler {
+public:
+  Upsampler2x(std::size_t blockSize);
 };
 } // namespace web_audio::detail
