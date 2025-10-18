c4006b6eaaf0e9a1f24664409485975a79839642
k0michi <k0michi@koyomi.co>
2025-10-18 10:06:28 +0900

    Fix WaveShaperOptions

--- internal/web-audio-cpp/include/web_audio/wave_shaper_options.hh
+++ internal/web-audio-cpp/include/web_audio/wave_shaper_options.hh
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <optional>
 #include <vector>
 
+#include "audio_node_options.hh"
 #include "over_sample_type.hh"
 
 namespace web_audio {
-struct WaveShaperOptions {
-  std::vector<float> curve;
+struct WaveShaperOptions : public AudioNodeOptions {
+  std::optional<std::vector<float>> curve;
   OverSampleType oversample = OverSampleType::eNone;
 };
 } // namespace web_audio
