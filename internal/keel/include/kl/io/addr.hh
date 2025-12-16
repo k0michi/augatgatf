6536a1e9ebbcb6ad1dd51e81b2d600064a10238a
k0michi <k0michi@koyomi.co>
2025-12-16 16:01:23 +0900

    Add Addr

---
+++ internal/keel/include/kl/io/addr.hh
@@ -0,0 +1,68 @@
+#ifndef KL_IO_ADDR_HH
+#define KL_IO_ADDR_HH
+
+#include <string>
+
+#include <uv.h>
+
+#include "kl/io/io_exception.hh"
+
+namespace kl::io {
+using PosixAddr = struct sockaddr;
+using PosixAddrStorage = struct sockaddr_storage;
+
+enum class AddrFamily {
+  eIPv4 = AF_INET,
+  eIPv6 = AF_INET6,
+};
+
+struct Addr {
+  static Expected<Addr> createIPv4(const std::u8string &ip, uint16_t port) {
+    Addr addr;
+    addr.family_ = AddrFamily::eIPv4;
+    addr.ip_ = ip;
+    addr.port_ = port;
+    auto result =
+        uv_ip4_addr(reinterpret_cast<const char *>(ip.c_str()), port,
+                    reinterpret_cast<struct sockaddr_in *>(&addr.nativeAddr_));
+
+    if (result < 0) {
+      return std::unexpected(
+          IOException(std::string("Failed to create IPv4 address: ") +
+                      uv_strerror(result)));
+    }
+
+    return addr;
+  }
+
+  static Expected<Addr> createIPv6(const std::u8string &ip, uint16_t port) {
+    Addr addr;
+    addr.family_ = AddrFamily::eIPv6;
+    addr.ip_ = ip;
+    addr.port_ = port;
+
+    auto result =
+        uv_ip6_addr(reinterpret_cast<const char *>(ip.c_str()), port,
+                    reinterpret_cast<struct sockaddr_in6 *>(&addr.nativeAddr_));
+
+    return addr;
+  }
+
+  AddrFamily family() const { return family_; }
+
+  std::u8string_view ip() const { return ip_; }
+
+  uint16_t port() const { return port_; }
+
+  const PosixAddr *posixAddr() const {
+    return reinterpret_cast<const PosixAddr *>(&nativeAddr_);
+  }
+
+private:
+  AddrFamily family_;
+  std::u8string ip_;
+  uint16_t port_;
+  PosixAddrStorage nativeAddr_;
+};
+} // namespace kl::io
+#endif
---
+++ internal/keel/test/test_addr.cc
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+#include "kl/io/addr.hh"
+
+using namespace kl::io;
+
+TEST(AddrTest, CreateIPv4) {
+  auto result = Addr::createIPv4(u8"0.0.0.0", 80);
+  ASSERT_TRUE(result.has_value());
+  Addr addr = result.value();
+  EXPECT_EQ(addr.family(), AddrFamily::eIPv4);
+  EXPECT_EQ(addr.ip(), u8"0.0.0.0");
+  EXPECT_EQ(addr.port(), 80);
+}
+
+TEST(AddrTest, CreateIPv6) {
+  auto result = Addr::createIPv6(u8"::1", 80);
+  ASSERT_TRUE(result.has_value());
+  Addr addr = result.value();
+  EXPECT_EQ(addr.family(), AddrFamily::eIPv6);
+  EXPECT_EQ(addr.ip(), u8"::1");
+  EXPECT_EQ(addr.port(), 80);
+}
+
+TEST(AddrTest, CreateInvalidIPv4) {
+  auto result = Addr::createIPv4(u8"999.999.999.999", 80);
+  ASSERT_FALSE(result.has_value());
+}
+
+TEST(AddrTest, CreateInvalidIPv6) {
+  auto result = Addr::createIPv6(u8"gggg::gggg", 80);
+  ASSERT_TRUE(result.has_value());
+}
