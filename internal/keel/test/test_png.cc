4967735047a3d83a1e4c387b576affd87ba54bf3
k0michi <k0michi@koyomi.co>
2025-12-19 12:47:28 +0900

    Add ReadInvalidPngImage test

--- internal/keel/test/test_png.cc
+++ internal/keel/test/test_png.cc
@@ -2,6 +2,7 @@
 
 #include "kl/assets/png.hh"
 #include "kl/io/file.hh"
+#include "kl/io/memory_stream.hh"
 
 class PngTest : public ::testing::Test {
 protected:
@@ -20,6 +21,23 @@ TEST_F(PngTest, LoadPngImage) {
     EXPECT_EQ(image.format(), kl::assets::ColorFormat::eR8G8B8A8Unorm);
     EXPECT_EQ(image.extent().width, 16u);
     EXPECT_EQ(image.extent().height, 16u);
+  }();
+  loop->run();
+}
+
+TEST_F(PngTest, ReadInvalidPngImage) {
+  [&]() -> kl::concurrent::Task<void> {
+    std::vector<std::byte> data{std::byte{'N'}, std::byte{'o'}, std::byte{'t'},
+                                std::byte{' '}, std::byte{'a'}, std::byte{' '},
+                                std::byte{'P'}, std::byte{'N'}, std::byte{'G'},
+                                std::byte{' '}, std::byte{'f'}, std::byte{'i'},
+                                std::byte{'l'}, std::byte{'e'}};
+    kl::io::MemoryStream stream;
+    co_await stream.write(loop,
+                          std::span<const std::byte>(data.data(), data.size()));
+    co_await stream.seek(loop, 0, kl::io::SeekDirection::eBegin);
+    auto imageExpected = co_await kl::assets::readPNG(loop, stream);
+    EXPECT_FALSE(imageExpected.has_value());
   }();
   loop->run();
 }
